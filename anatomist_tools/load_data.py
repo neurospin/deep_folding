9beaf46921a8faa7075a9fd1b297cbc569408acc
Louise GUILLON <lg261972@is234205.intra.cea.fr>
2021-02-08 19:14:55 +0100

    addition of comments

--- anatomist_tools/load_data.py
+++ anatomist_tools/load_data.py
@@ -1,3 +1,7 @@
+"""
+Scripts that enables to create a dataframe of numpy arrays from .nii.gz or .nii
+images.
+"""
 from __future__ import division
 import os
 
@@ -5,7 +9,6 @@ import anatomist.api as anatomist
 from soma import aims
 a = anatomist.Anatomist()
 
-#from pynet_transforms import *
 import pandas as pd
 import numpy as np
 import re
@@ -25,7 +28,6 @@ def fetch_data(root_dir):
     data = ['train', 'test']
     for phase in data:
         phase=''
-        #skeleton_array = []
         data_dict = dict()
         for filename in os.listdir(root_dir+phase):
             file = os.path.join(root_dir+phase, filename)
@@ -43,20 +45,10 @@ def fetch_data(root_dir):
 
                 data_dict[filename] = [sample]
                 print(filename)
-                #skeleton_array.append(sample)
 
         dataframe = pd.DataFrame.from_dict(data_dict)
-
-        # skeleton
-        # dataframe.to_pickle('/neurospin/dico/lguillon/data/data_skeleton_saved_1mm_%s.pkl' % phase)
-        # dataframe.to_pickle('/neurospin/dico/lguillon/data/sk_stripped_crop_2_%s.pkl' % phase)
-        # dataframe.to_pickle('/neurospin/dico/lguillon/data/crop_norm_spm_%s.pkl' % phase)
-        # dataframe.to_pickle('/neurospin/dico/lguillon/data/crop_norm_spm_skel_%s.pkl' % phase)
-        # dataframe.to_pickle('/neurospin/dico/lguillon/hcp_cs_crop/exp_crop_size/shift_minus5_xy/shift_minus5_xy_%s.pkl' % phase)
-        # dataframe.to_pickle('/neurospin/dico/lguillon/hcp_cs_crop/sts_crop/sts_crop.pkl')
-        # dataframe.to_pickle('/neurospin/dico/lguillon/aims_detection/aims_crop/skeleton/sts_crop_skeleton_right.pkl')
         dataframe.to_pickle('/neurospin/dico/lguillon/mic21/anomalies_set/dataset/benchmark2/abnormal_skeleton_left.pkl')
-        #np.save(root_dir+'_skeleton_saved_%s' % phase, skeleton_array)
+
 
 if __name__ == '__main__':
     input = 'skeleton'
@@ -64,18 +56,8 @@ if __name__ == '__main__':
 
     if input == 'raw':
         # Raw MRIs - crop
-        # fetch_data('/home/lg261972/manip/hcp_cs/hcp_cs_sep/data/')
-        # Crop with same procedure as skeletons
-        #fetch_data('/neurospin/dico/lguillon/hcp_cs_crop/sk_stripped_')
-        # fetch_data('/neurospin/dico/lguillon/hcp_cs_crop/v2/')
-        # fetch_data('/neurospin/dico/lguillon/hcp_cs_crop/v4_norm_spm/')
-        #fetch_data('/neurospin/dico/lguillon/hcp_cs_crop/exp_crop_size/shift_minus5_xy/')
-        # fetch_data('/neurospin/dico/lguillon/hcp_cs_crop/sts_crop/')
-        # fetch_data('/neurospin/dico/lguillon/skeleton/sts_crop/right_hemi/normalized_crop/')
         fetch_data('/neurospin/dico/lguillon/aims_detection/aims_crop/skeleton/right_hemi/normalized_crops/')
 
     else:
         # Skeletons
-        #fetch_data('/neurospin/dico/lguillon/skeleton/skeleton_crop_')
-        #fetch_data('/neurospin/dico/lguillon/skeleton/normalized_skeleton/')
         fetch_data('/neurospin/dico/lguillon/mic21/anomalies_set/dataset/benchmark2/0_Lside/')
