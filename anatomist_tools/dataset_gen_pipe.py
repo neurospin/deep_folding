191d1d43d397c1af7342bcb2f5c6586c3b019ab3
Louise GUILLON <lg261972@is234205.intra.cea.fr>
2021-02-26 16:29:31 +0100

    modification in order to make AimsSubVolume depend on bbox value

--- anatomist_tools/dataset_gen_pipe.py
+++ anatomist_tools/dataset_gen_pipe.py
@@ -12,12 +12,15 @@ side = 'L' # hemisphere 'L' or 'R'
 bbox = ([105, 109, 23], [147, 171, 93]) # Bounding box defined thanks to
                                         # bbox_definition.py for S.T.s ter. asc.
                                         # ant. and post.
+xmin, ymin, zmin = str(bbox[0][0]), str(bbox[0][1]), str(bbox[0][2])
+xmax, ymax, zmax = str(bbox[1][0]), str(bbox[1][1]), str(bbox[1][2])
+str_box = ' -x ' + xmin + ' -y ' + ymin + ' -z ' + zmin + ' -X '+ xmax + ' -Y ' + ymax + ' -Z ' + zmax
 
 dir = '/neurospin/hcp/ANALYSIS/3T_morphologist/'
 comp_dir = '/t1mri/default_acquisition/default_analysis/segmentation/' + side + 'skeleton_'
 new_dir = '/neurospin/dico/deep_folding/data/' +  side + 'crops/'
 
-for sub in os.listdir(dir): # go through all HCP subjects folder
+for sub in os.listdir(dir)[:2]: # go through all HCP subjects folder
 
     # Normalization and resampling of skeleton images
     dir_m = '/neurospin/dico/lguillon/skeleton/transfo_pre_process/natif_to_template_spm_' + sub +'.trm'
@@ -27,7 +30,7 @@ for sub in os.listdir(dir): # go through all HCP subjects folder
 
     # Crop of the images based on bouding box
     file = new_dir + sub + '_normalized.nii.gz'
-    cmd_crop = 'AimsSubVolume -i ' + file + ' -o ' + file + ' -x 105 -y 109 -z 23 -X 147 -Y 171 -Z 93'
+    cmd_crop = 'AimsSubVolume -i ' + file + ' -o ' + file + str_box
     os.system(cmd_crop)
 
 # Creation of .pickle file
