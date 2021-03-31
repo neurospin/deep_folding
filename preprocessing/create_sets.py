505e2389d7f4281342f4fc2b74c300efb4d71d15
Louise GUILLON <lg261972@is234205.intra.cea.fr>
2021-03-31 15:33:22 +0200

    suppression of create_aims_sets function to put it in abnorFolding repo

--- preprocessing/create_sets.py
+++ preprocessing/create_sets.py
@@ -1,5 +1,38 @@
+# -*- coding: utf-8 -*-
 # /usr/bin/env python3
-# coding: utf-8
+#
+#  This software and supporting documentation are distributed by
+#      Institut Federatif de Recherche 49
+#      CEA/NeuroSpin, Batiment 145,
+#      91191 Gif-sur-Yvette cedex
+#      France
+#
+# This software is governed by the CeCILL license version 2 under
+# French law and abiding by the rules of distribution of free software.
+# You can  use, modify and/or redistribute the software under the
+# terms of the CeCILL license version 2 as circulated by CEA, CNRS
+# and INRIA at the following URL "http://www.cecill.info".
+#
+# As a counterpart to the access to the source code and  rights to copy,
+# modify and redistribute granted by the license, users are provided only
+# with a limited warranty  and the software's author,  the holder of the
+# economic rights,  and the successive licensors  have only  limited
+# liability.
+#
+# In this respect, the user's attention is drawn to the risks associated
+# with loading,  using,  modifying and/or developing or reproducing the
+# software by the user in light of its specific status of free software,
+# that may mean  that it is complicated to manipulate,  and  that  also
+# therefore means  that it is reserved for developers  and  experienced
+# professionals having in-depth computer knowledge. Users are therefore
+# encouraged to load and test the software's suitability as regards their
+# requirements in conditions enabling the security of their systems and/or
+# data to be ensured and,  more generally, to use and operate it in the
+# same conditions as regards security.
+#
+# The fact that you are presently reading this means that you have had
+# knowledge of the CeCILL license version 2 and that you accept its terms.
+
 """
 The aim of this script is to create pytorch dataloaders from MRIs saved as
 numpy arrays in a .pickle.
@@ -16,13 +49,13 @@ import torchvision.transforms as transforms
 from torch.autograd import Variable
 import torchio as tio
 
-from dl_tools.pynet_transforms import *
-from dl_tools import save_results
+# from dl_tools.pynet_transforms import *
+# from dl_tools import save_results
 
 
 class TensorDataset():
     """Custom dataset that includes image file paths.
-    Apply different transformations to data depending on the type of input.
+    Applies different transformations to data depending on the type of input.
     IN: data_tensor: tensor containing MRIs as numpy arrays
         filenames: list of subjects' IDs
         skeleton: boolean, whether input is skeleton images or not
@@ -109,8 +142,8 @@ class SkeletonDataset():
 
 class AugDatasetTransformer(torch.utils.data.Dataset):
     """
-    Custom dataset that apply data augmentation on a dataset processed
-    through TensorDataset class.
+    Custom dataset that applies data augmentation on a dataset processed
+    through TensorDataset or Skeleton Dataset classes.
     Transformations are performed on CPU.
     """
     def __init__(self, base_dataset):
@@ -333,84 +366,3 @@ def create_left_handed_set(skeleton, side):
                                                     shuffle=True, num_workers=0)
 
     return leftH_loader
-
-
-
-def create_aims_sets(skeleton, side, handedness=0):
-    """
-    Creates datasets from AIMS data
-    IN: skeleton: boolean, True if input is skeleton, False otherwise,
-        side: str, 'right' or 'left'
-        handedness: int, 0 if mixed ind, 1 if right handed, 2 if left handed
-    OUT:
-    """
-
-    if handedness == 0:
-        if skeleton:
-            controls = pd.read_pickle('/neurospin/dico/lguillon/aims_detection/aims_crop/skeleton/{}_hemi/controls_qc_{}.pkl'.format(side, side))
-            asd = pd.read_pickle('/neurospin/dico/lguillon/aims_detection/aims_crop/skeleton/{}_hemi/asd_qc_{}.pkl'.format(side, side))
-            id_controls = pd.read_pickle('/neurospin/dico/lguillon/aims_detection/aims_crop/skeleton/{}_hemi/id_controls_qc_{}.pkl'.format(side, side))
-            asd_id = pd.read_pickle('/neurospin/dico/lguillon/aims_detection/aims_crop/skeleton/{}_hemi/asd_id_qc_{}.pkl'.format(side, side))
-        else:
-            controls = pd.read_pickle('/neurospin/dico/lguillon/aims_detection/aims_crop/controls_qc_{}.pkl'.format(side))
-            asd = pd.read_pickle('/neurospin/dico/lguillon/aims_detection/aims_crop/asd_qc_{}.pkl'.format(side))
-            id_controls = pd.read_pickle('/neurospin/dico/lguillon/aims_detection/aims_crop/id_controls_qc_{}.pkl'.format(side))
-            asd_id = pd.read_pickle('/neurospin/dico/lguillon/aims_detection/aims_crop/asd_id_qc_{}.pkl'.format(side))
-        asd = asd.rename(columns={0:'mri'})
-        asd_id = asd_id.rename(columns={0:'mri'})
-        controls = controls.rename(columns={0:'mri'})
-        id_controls = id_controls.rename(columns={0:'mri'})
-    else:
-        if handedness==1:
-            hand = 'right'
-        else:
-            hand = 'left'
-        if skeleton:
-            #controls = pd.read_pickle('/neurospin/dico/lguillon/aims_detection/aims_crop/skeleton/{}_hemi/controls_qc_{}_{}H.pkl'.format(side, side, hand))
-            #asd = pd.read_pickle('/neurospin/dico/lguillon/aims_detection/aims_crop/skeleton/{}_hemi/asd_qc_{}_{}H.pkl'.format(side, side, hand))
-            #id_controls = pd.read_pickle('/neurospin/dico/lguillon/aims_detection/aims_crop/skeleton/{}_hemi/id_controls_qc_{}_{}H.pkl'.format(side, side, hand))
-            #asd_id = pd.read_pickle('/neurospin/dico/lguillon/aims_detection/aims_crop/skeleton/{}_hemi/asd_id_qc_{}_{}H.pkl'.format(side, side, hand))
-
-            controls = pd.read_pickle('/home_local/lg261972/data/data_handedness/{}_hemi/controls_qc_{}_{}H.pkl'.format(side, side, hand))
-            asd = pd.read_pickle('/home_local/lg261972/data/data_handedness/{}_hemi/asd_qc_{}_{}H.pkl'.format(side, side, hand))
-            id_controls = pd.read_pickle('/home_local/lg261972/data/data_handedness/{}_hemi/id_controls_qc_{}_{}H.pkl'.format(side, side, hand))
-            asd_id = pd.read_pickle('/home_local/lg261972/data/data_handedness/{}_hemi/asd_id_qc_{}_{}H.pkl'.format(side, side, hand))
-        else:
-            controls = pd.read_pickle('/neurospin/dico/lguillon/aims_detection/aims_crop/{}_hemi/controls_qc_{}_{}H.pkl'.format(side, side, hand))
-            asd = pd.read_pickle('/neurospin/dico/lguillon/aims_detection/aims_crop/{}_hemi/asd_qc_{}_{}H.pkl'.format(side, side, hand))
-            id_controls = pd.read_pickle('/neurospin/dico/lguillon/aims_detection/aims_crop/{}_hemi/id_controls_qc_{}_{}H.pkl'.format(side, side, hand))
-            asd_id = pd.read_pickle('/neurospin/dico/lguillon/aims_detection/aims_crop/{}_hemi/asd_id_qc_{}_{}H.pkl'.format(side, side, hand))
-
-    filenames_asd = asd['ID']
-    asd = torch.from_numpy(np.array([asd.mri.values[k] for k in range(len(asd))]))
-    asd = asd.to('cuda')
-    asd_dataset = TensorDataset(filenames=filenames_asd, data_tensor=asd,
-                                skeleton=skeleton, vae=False)
-    asd_dataset = torch.utils.data.DataLoader(asd_dataset, shuffle=True,
-                                              num_workers=0, batch_size=1)
-
-    filenames_controls = controls['ID']
-    controls = torch.from_numpy(np.array([controls.mri.values[k] for k in range(len(controls))]))
-    controls = controls.to('cuda')
-    controls_dataset = TensorDataset(filenames=filenames_controls, data_tensor=controls,
-                                    skeleton=skeleton, vae=False)
-    controls_dataset = torch.utils.data.DataLoader(controls_dataset, shuffle=True,
-                                              num_workers=0, batch_size=1)
-
-    filenames_id_controls = id_controls['ID']
-    id_controls = torch.from_numpy(np.array([id_controls.mri.values[k] for k in range(len(id_controls))]))
-    id_controls = id_controls.to('cuda')
-    id_controls_dataset = TensorDataset(filenames=filenames_id_controls, data_tensor=id_controls,
-                                        skeleton=skeleton, vae=False)
-    id_controls_dataset = torch.utils.data.DataLoader(id_controls_dataset, shuffle=True,
-                                              num_workers=0, batch_size=1)
-
-    filenames_asd_id = asd_id['ID']
-    asd_id = torch.from_numpy(np.array([asd_id.mri.values[k] for k in range(len(asd_id))]))
-    asd_id = asd_id.to('cuda')
-    asd_id_dataset = TensorDataset(filenames=filenames_asd_id, data_tensor=asd_id,
-                                   skeleton=skeleton, vae=False)
-    asd_id_dataset = torch.utils.data.DataLoader(asd_id_dataset, shuffle=True,
-                                              num_workers=0, batch_size=1)
-
-    return asd_dataset, controls_dataset, id_controls_dataset, asd_id_dataset
