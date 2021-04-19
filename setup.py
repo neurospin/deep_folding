20b6bb41360be3d3c8fb8c60c44c01a1baf6186f
Joël <joel.chavas@gmail.com>
2021-04-19 15:15:28 +0200

    Modification of setup files for consistency

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name='deep_folding',
     version='0.0.1',
-    packages=find_packages(exclude=['tests*', 'examples*']),
+    packages=find_packages(exclude=['tests*', 'notebooks*']),
     license='CeCILL license version 2',
     description='Deep learning utilities to characterize sulcus patterns',
     long_description=open('README.rst').read(),
     install_requires=['six', 'numpy', 'pytest', 'GitPython', 'typing'],
     url='https://github.com/neurospin/deep_folding',
-    author='',
+    author='Louise Guillon and Joël Chavas',
     author_email=''
 )
