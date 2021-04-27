e89fea58add4a8cef2b9d7c1b69c276a0849ee7c
Joël <joel.chavas@gmail.com>
2021-04-27 19:20:09 +0200

    Adds interpolation option. Tests nearest neighbour interpolation using AimsApplyTransform

--- tests/test_dataset_gen_pipe.py
+++ tests/test_dataset_gen_pipe.py
@@ -7,6 +7,7 @@ from soma import aims
 
 from deep_folding.anatomist_tools import dataset_gen_pipe
 
+_ALL_SUBJECTS = -1
 
 def are_arrays_almost_equal(arr1, arr2, epsilon, max_number_different_pixels):
 	"""Returns True if arrays arr1 and arr2 are almost equal
@@ -29,7 +30,7 @@ def are_arrays_almost_equal(arr1, arr2, epsilon, max_number_different_pixels):
 	equal_arrays = number_different_pixels <= max_number_different_pixels
 	return equal_arrays, number_different_pixels
 
-def test_dataset_gen_pipe():
+def test_dataset_gen_pipe_linear():
 	"""Tests if the dataset generation on one subject gives the expected result.
 
 	The source and the reference are in the data subdirectory
@@ -40,7 +41,7 @@ def test_dataset_gen_pipe():
 	src_dir = os.path.abspath(src_dir)
 
 	# Gets the reference, transform and bbox directory
-	ref_dir = os.path.join(os.getcwd(), 'data/reference/data')
+	ref_dir = os.path.join(os.getcwd(), 'data/reference/data/linear')
 	ref_dir = os.path.abspath(ref_dir)
 	print("ref_dir = " + ref_dir)
 	transform_dir = os.path.join(os.getcwd(), 'data/reference/transform')
@@ -49,7 +50,7 @@ def test_dataset_gen_pipe():
 	bbox_dir = os.path.abspath(bbox_dir)
 
 	# Defines the target directory
-	tgt_dir = os.path.join(os.getcwd(), 'data/target/data')
+	tgt_dir = os.path.join(os.getcwd(), 'data/target/data/linear')
 	tgt_dir = os.path.abspath(tgt_dir)
 
 	# Gets sulcus name and hemisphere side
@@ -64,7 +65,63 @@ def test_dataset_gen_pipe():
 		bbox_dir=bbox_dir,
 		list_sulci=sulcus,
 		side=side,
-		number_subjects=dataset_gen_pipe._ALL_SUBJECTS)
+		interp='linear',
+		number_subjects=_ALL_SUBJECTS)
+
+	# Gets target crop as numpy array
+	cropped_target_dir = os.path.join(tgt_dir, side+'crops')
+	vol_target_file = glob.glob(cropped_target_dir + '/' + '*.nii.gz')
+	vol_target = aims.read(vol_target_file[0])
+	arr_target = vol_target.arraydata()
+
+	# Gets reference crop as numpy array
+	cropped_ref_dir = os.path.join(ref_dir, side+'crops')
+	vol_ref_file = glob.glob(cropped_ref_dir + '/' + '*.nii.gz')
+	vol_ref = aims.read(vol_ref_file[0])
+	arr_ref = vol_ref.arraydata()
+
+	# Test fails if arrays differ strictly of more than 1 on more than 2 pixels
+	equal_arrays, number_different_pixels = \
+		are_arrays_almost_equal(arr_ref, arr_target, 1, 2)
+	assert equal_arrays
+
+def test_dataset_gen_pipe_nearest():
+	"""Tests if the dataset generation on one subject gives the expected result.
+
+	The source and the reference are in the data subdirectory
+	"""
+
+	# Gets the source directory
+	src_dir = os.path.join(os.getcwd(), 'data/source/unsupervised')
+	src_dir = os.path.abspath(src_dir)
+
+	# Gets the reference, transform and bbox directory
+	ref_dir = os.path.join(os.getcwd(), 'data/reference/data/nearest')
+	ref_dir = os.path.abspath(ref_dir)
+	print("ref_dir = " + ref_dir)
+	transform_dir = os.path.join(os.getcwd(), 'data/reference/transform')
+	transform_dir = os.path.abspath(transform_dir)
+	bbox_dir = os.path.join(os.getcwd(), 'data/reference/bbox')
+	bbox_dir = os.path.abspath(bbox_dir)
+
+	# Defines the target directory
+	tgt_dir = os.path.join(os.getcwd(), 'data/target/data/nearest')
+	tgt_dir = os.path.abspath(tgt_dir)
+
+	# Gets sulcus name and hemisphere side
+	sulcus = 'S.T.s.ter.asc.ant._left'
+	side = 'L'
+
+	# Determines the bounding box around the sulcus
+	dataset_gen_pipe.dataset_gen_pipe(
+		src_dir=src_dir,
+		tgt_dir=tgt_dir,
+		transform_dir=transform_dir,
+		bbox_dir=bbox_dir,
+		list_sulci=sulcus,
+		side=side,
+		interp='linear',
+		number_subjects=_ALL_SUBJECTS)
 
 	# Gets target crop as numpy array
 	cropped_target_dir = os.path.join(tgt_dir, side+'crops')
