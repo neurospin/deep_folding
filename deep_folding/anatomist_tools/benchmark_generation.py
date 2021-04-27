1c69c29b42307e39f4016a14881c4abfa947ba4e
Louise GUILLON <lg261972@is234205.intra.cea.fr>
2021-04-27 14:52:29 +0200

    new function to create benchmark merge

--- deep_folding/anatomist_tools/benchmark_generation.py
+++ deep_folding/anatomist_tools/benchmark_generation.py
@@ -56,6 +56,17 @@ import os
 import utils.load_bbox
 
 
+def get_sub_list():
+    """Returns subjects list for which latered skelerons can be created
+    Only right handed HCP subjects
+    """
+    right_handed = pd.read_csv('/neurospin/dico/lguillon/hcp_info/right_handed.csv')
+    subjects_list = list(right_handed['Subject'])
+    random.shuffle(subjects_list)
+
+    return subjects_list
+
+
 def generate(b_num, side, ss_size, sulci_list):
     """ Generates the abnormal skeleton images
 
@@ -80,7 +91,7 @@ def generate(b_num, side, ss_size, sulci_list):
     # bounding box defined for the crop and including at least ss_size voxels
 
     abnormality_test = []
-    bbmin, bbmax = utils.load_bbox.load(sulci_list, side, talairach_box=True)
+    bbmin, bbmax = utils.load_bbox.compute_max_box(sulci_list, side, talairach_box=True)
     print(bbmin, bbmax)
 
     for sub in subjects_list:
@@ -141,9 +152,95 @@ def generate(b_num, side, ss_size, sulci_list):
     df_abnor_test.to_csv(saving_dir + 'abnormality_test.csv')
 
 
+def generate_add_ss(b_num, side, ss_size, sulci_list):
+    """
+    Creates a benchmark with skeletons having additional simple surfaces (ss)
+    IN: b_num: benchmark number
+        side: hemisphere, str, whether 'L' or 'R',
+        region: region of the brain
+        ss_size: Minimal size of simple surface to suppress
+    OUT: altered skeletons generated
+         list of subjects altered or original
+    """
+    n = 150 # benchmark size
+    data_dir = '/neurospin/hcp/ANALYSIS/3T_morphologist/' # folder containing all HCP subjects folder
+    saving_dir = '/neurospin/dico/lguillon/mic21/anomalies_set/dataset/benchmark_merge/benchmark' + str(b_num) + '/'
+
+    subjects_list = get_sub_list()
+
+    abnormality_test, givers = [], []
+    bbmin, bbmax = utils.load_bbox.compute_max_box(sulci_list, side, talairach_box=True)
+    print(bbmin, bbmax)
+
+    for i, sub in enumerate(subjects_list):
+        print(sub)
+        if os.path.isdir(data_dir + str(sub)):
+            surfaces = dict()
+            graph = aims.read(data_dir + str(sub) + '/t1mri/default_acquisition/default_analysis/folds/3.1/default_session_auto/'+ side + str(sub) + '_default_session_auto.arg')
+
+            for v in graph.vertices():
+                if 'label' in v:
+                    bbmin_surface = v['Tal_boundingbox_min']
+                    bbmax_surface = v['Tal_boundingbox_max']
+                    bck_map = v['aims_ss']
+
+                    if all([a >= b for (a, b) in zip(bbmin_surface, bbmin)]) and all([a <= b for (a, b) in zip(bbmax_surface, bbmax)]):
+                        for bucket in bck_map:
+                            if bucket.size() > ss_size: # In order to keep only large enough simple surfaces
+                                surfaces[len(surfaces)] = v
+
+            if len(surfaces.keys())>0: # there is at least one SS that satisfies conditions
+                surface = random.randint(0, len(surfaces)-1)
+                print(surfaces[surface]['label'])
+
+                # Loading of skeleton on which SS will be added
+                sub_added = subjects_list[i+1]
+                if os.path.isdir(data_dir + str(sub_added)):
+                    skel = aims.read(data_dir + str(sub_added) + '/t1mri/default_acquisition/default_analysis/segmentation/' + side + 'skeleton_' + str(sub_added) + '.nii.gz')
+                    bck_map = surfaces[surface]['aims_ss']
+                    for voxel in bck_map[0].keys():
+                        if skel.value(voxel[0], voxel[1], voxel[2])!=11:
+                            skel.setValue(60, voxel[0], voxel[1], voxel[2])
+
+                    bck_map_bottom = surfaces[surface]['aims_bottom']
+                    for voxel in bck_map_bottom[0].keys():
+                        if skel.value(voxel[0], voxel[1], voxel[2])!=11:
+                            skel.setValue(60, voxel[0], voxel[1], voxel[2])
+
+                    # Writting of output graph
+                    fileout = saving_dir + 'output_skeleton_test_' + str(sub_added) + '.nii.gz'
+                    print('writing altered skeleton to', fileout)
+                    aims.write(skel, fileout)
+
+                    # Addition of modified graph to abnormality_test set
+                    abnormality_test.append(sub_added)
+                    givers.append(sub)
+                    if len(abnormality_test) == n:
+                        break
+
+    # Train, validation and test separation
+    # It is important that examples of this benchmark aren't used during training and
+    # validation phase. In order to make sure this is the case, 2 lists are created.
+    nor_set = list(set(subjects_list) - set(abnormality_test))
+    nor_set = list(set(nor_set)- set(givers))
+
+    print('Dataset split, train + normal test: ', len(nor_set),
+          'abnormal test: ', len(abnormality_test),
+          'givers: ', len(givers))
+
+    df_train = pd.DataFrame(nor_set)
+    df_train.to_csv(saving_dir + 'train.csv')
+
+    df_abnor_test = pd.DataFrame(abnormality_test)
+    df_abnor_test.to_csv(saving_dir + 'abnormality_test.csv')
+
+    df_givers = pd.DataFrame(givers)
+    df_givers.to_csv(saving_dir + 'givers.csv')
+
+
 if __name__ == '__main__':
-    benchmark_num = 7
+    b_num = 1
     side = 'L'
-    region = 'S.T.s'
-    ss_size = 600
-    generate(benchmark_num, side, ss_size, ['S.T.s.ter.asc.ant._left', 'S.T.s.ter.asc.test._left'])
+    sulci_list = ['S.T.s.ter.asc.ant._left', 'S.T.s.ter.asc.post._left']
+    ss_size = 500
+    generate_add_ss(b_num, side, ss_size, sulci_list)
