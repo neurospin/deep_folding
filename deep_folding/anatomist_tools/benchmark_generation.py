6b0b274f08e9c8060240693ffc1540b74757c337
Louise GUILLON <lg261972@is234205.intra.cea.fr>
2021-05-05 15:16:23 +0200

    re organization of code in order to be more flexible

--- deep_folding/anatomist_tools/benchmark_generation.py
+++ deep_folding/anatomist_tools/benchmark_generation.py
@@ -56,6 +56,89 @@ import os
 import utils.load_bbox
 
 
+class Benchmark():
+    def __init__(self, b_num, side, ss_size, sulci_list,
+                 data_dir=_DEFAULT_DATA_DIR, saving_dir=_DEFAULT_SAVING_DIR):
+        self.b_num = b_num
+        self.side = side
+        self.ss_size = ss_size
+        self.sulci_list = sulci_list
+        self.data_dir = data_dir
+        self.saving_dir = saving_dir
+        self.subjects_list = get_sub_list()
+        self.abnormality_test = []
+        self.bbmin, self.bbmax = utils.load_bbox.compute_max_box(sulci_list, side, talairach_box=True)
+        print(self.bbmin, self.bbmax)
+
+    def get_one_simple_surface(self, sub):
+
+        cpt_arg_1 = '/t1mri/default_acquisition/default_analysis/folds/3.1/default_session_auto/'
+        cpt_arg_2 = '_default_session_auto.arg'
+        cpt_skel_1 = '/t1mri/default_acquisition/default_analysis/segmentation/'
+        cpt_skel_2 = 'skeleton_'
+        cpt_skel_3 = '.nii.gz'
+
+        if os.path.isdir(self.data_dir + str(sub)):
+            surfaces = dict()
+
+            graph = aims.read(os.path.join(self.data_dir, str(sub),
+                              cpt_arg_1, side, str(sub), cpt_arg_2))
+            self.skel = aims.read(os.path.join(self.data_dir, str(sub), cpt_skel_1,
+                            side, cpt_skel_2, str(sub), cpt_skel_3))
+
+            for v in graph.vertices():
+                if 'label' in v:
+                    bbmin_surface = v['Tal_boundingbox_min']
+                    bbmax_surface = v['Tal_boundingbox_max']
+                    bck_map = v['aims_ss']
+
+                    if all([a >= b for (a, b) in zip(bbmin_surface, self.bbmin)]) and all([a <= b for (a, b) in zip(bbmax_surface, self.bbmax)]):
+                        for bucket in bck_map:
+                            if bucket.size() > self.ss_size: # In order to keep only large enough simple surfaces
+                                surfaces[len(surfaces)] = v
+
+            print(len(surfaces.keys()))
+        return surfaces
+
+
+    def alter_skeleton(self):
+
+        # Suppression of one random simple surface (satisfying both criteria)
+        surface = random.randint(0, len(surfaces)-1)
+        print(surfaces[surface]['label'])
+
+        bck_map = surfaces[surface]['aims_ss']
+        for voxel in bck_map[0].keys():
+            self.skel.setValue(0, voxel[0], voxel[1], voxel[2])
+
+        bck_map_bottom = surfaces[surface]['aims_bottom']
+        for voxel in bck_map_bottom[0].keys():
+            self.skel.setValue(0, voxel[0], voxel[1], voxel[2])
+
+        return skel
+
+
+    def save_file(self):
+        # Writting of output graph
+        fileout = self.saving_dir + 'output_skeleton_' + str(sub) + '.nii.gz'
+        print('writing altered skeleton to', fileout)
+        aims.write(self.skel, fileout)
+
+
+    def save_lists(self, abnormality_test):
+        nor_set = list(set(subjects_list) - set(abnormality_test))
+
+        print('Dataset split, train + val + normal test: ', len(nor_set),
+              'abnormal test: ', len(abnormality_test))
+
+        df_train = pd.DataFrame(nor_set)
+        df_train.to_csv(saving_dir + 'train.csv')
+
+        df_abnor_test = pd.DataFrame(abnormality_test)
+        df_abnor_test.to_csv(saving_dir + 'abnormality_test.csv')
+
+
+
 def get_sub_list():
     """Returns subjects list for which latered skelerons can be created
     Only right handed HCP subjects
@@ -67,8 +150,30 @@ def get_sub_list():
     return subjects_list
 
 
+def generate(b_num, side, ss_size, sulci_list, mode='suppress'):
+    b_num =111
+    benchmark = Benchmark(b_num, side, ss_size, sulci_list)
+    abnormality_test = []
+    for sub in subjects_list:
+        print(sub)
+        surfaces = benchmark.get_one_simple_surface(sub)
+        if len(surfaces.keys()) > 0:
+            benchmark.alter_skeleton(surfaces)
+            # Addition of modified graph to abnormality_test set
+            abnormality_test.append(sub)
+            if len(abnormality_test) == 150:
+                break
+
+        if test==False:
+            benchmark.save_file()
+            benchmark.save_lists(abnormality_test)
+
+
+
+
+"""
 def generate(b_num, side, ss_size, sulci_list):
-    """ Generates the abnormal skeleton images
+    Generates the abnormal skeleton images
 
     Args: b_num: the benchmark number to create (int)
           side: hemisphere, str, whether 'L' or 'R'
@@ -77,19 +182,16 @@ def generate(b_num, side, ss_size, sulci_list):
 
     Returns: list of subjects altered or original
              saves abnormal skeletons to saving_dir
-    """
+
     # folder containing all HCP subjects folder
     data_dir = '/neurospin/hcp/ANALYSIS/3T_morphologist/'
     saving_dir = '/neurospin/dico/lguillon/mic21/anomalies_set/dataset/benchmark' + str(b_num) +'/'
 
     # List of right handed subjects
-    right_handed = pd.read_csv('/neurospin/dico/lguillon/hcp_info/right_handed.csv')
-    subjects_list = list(right_handed['Subject'])
-    random.shuffle(subjects_list)
+    subjects_list = get_sub_list()
 
     # Saving of simple surfaces satisfying both criteria: completely inside the
     # bounding box defined for the crop and including at least ss_size voxels
-
     abnormality_test = []
     bbmin, bbmax = utils.load_bbox.compute_max_box(sulci_list, side, talairach_box=True)
     print(bbmin, bbmax)
@@ -149,7 +251,7 @@ def generate(b_num, side, ss_size, sulci_list):
     df_train.to_csv(saving_dir + 'train.csv')
 
     df_abnor_test = pd.DataFrame(abnormality_test)
-    df_abnor_test.to_csv(saving_dir + 'abnormality_test.csv')
+    df_abnor_test.to_csv(saving_dir + 'abnormality_test.csv')"""
 
 
 def generate_add_ss(b_num, side, ss_size, sulci_list):
@@ -237,10 +339,10 @@ def generate_add_ss(b_num, side, ss_size, sulci_list):
     df_givers = pd.DataFrame(givers)
     df_givers.to_csv(saving_dir + 'givers.csv')
 
-
+"""
 if __name__ == '__main__':
     b_num = 1
     side = 'L'
     sulci_list = ['S.T.s.ter.asc.ant._left', 'S.T.s.ter.asc.post._left']
     ss_size = 500
-    generate_add_ss(b_num, side, ss_size, sulci_list)
+    generate_add_ss(b_num, side, ss_size, sulci_list)"""
