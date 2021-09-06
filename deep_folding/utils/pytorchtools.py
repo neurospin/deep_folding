c1ff85416aaa66974e96f685f75dc60b51afa1df
louise-guillon <louise.guillon.fondimare@gmail.com>
2021-09-06 17:08:51 +0200

    bug fix with root dir and print when saving

--- deep_folding/utils/pytorchtools.py
+++ deep_folding/utils/pytorchtools.py
@@ -7,7 +7,7 @@ import torch
 
 class EarlyStopping:
     """Early stops the training if validation loss doesn't improve after a given patience."""
-    def __init__(self, patience=7, verbose=False, delta=0):
+    def __init__(self, root_dir, patience=7, verbose=False, delta=0):
         """
         Args:
             patience (int): How long to wait after last time validation loss improved.
@@ -24,28 +24,27 @@ class EarlyStopping:
         self.early_stop = False
         self.val_loss_min = np.Inf
         self.delta = delta
+        self.root_dir = root_dir
 
-    def __call__(self, val_loss, model, root_dir):
+    def __call__(self, val_loss, model):
 
         score = -val_loss
-
         if self.best_score is None:
             self.best_score = score
-            self.save_checkpoint(val_loss, model, root_dir)
+            self.save_checkpoint(val_loss, model, self.root_dir)
         elif score < self.best_score + self.delta:
             self.counter += 1
             print('EarlyStopping counter: {self.counter} out of {self.patience}')
             if self.counter >= self.patience:
                 self.early_stop = True
         else:
-            print('la saving')
             self.best_score = score
-            self.save_checkpoint(val_loss, model, root_dir)
+            self.save_checkpoint(val_loss, model, self.root_dir)
             self.counter = 0
 
     def save_checkpoint(self, val_loss, model, root_dir):
         '''Saves model when validation loss decrease.'''
         if self.verbose:
-            print('Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
-        torch.save(model.state_dict(), root_dir+'checkpoint.pt')
+            print(f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
+        torch.save(model.state_dict(), self.root_dir+'checkpoint.pt')
         self.val_loss_min = val_loss
