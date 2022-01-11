bd004a10db19ecc6336b7d2b00fc3ed00655ad94
JoelChavas <joel.chavas@gmail.com>
2022-01-11 14:31:29 +0100

    Adds sphinx requirements

--- deep_folding/info.py
+++ deep_folding/info.py
@@ -42,7 +42,8 @@ AUTHOR_EMAIL = ""
 PLATFORMS = "OS Independent"
 PROVIDES = ["deep_folding"]
 REQUIRES = ['six', 'numpy', 'pytest', 'GitPython', 'typing', 'joblib',
-            'tqdm>=4.36', 'pqdm']  # , 'moving_averages']
+            'tqdm>=4.36', 'pqdm', 'sphinx',
+            'sphinx-rtd-theme']  # , 'moving_averages']
 EXTRA_REQUIRES = {
     "plotting": ["matplotlib"],
     "doc": ["sphinx>=" + SPHINX_MIN_VERSION]}
