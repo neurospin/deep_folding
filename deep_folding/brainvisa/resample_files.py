b1cba43090df9a040d7d2150a6fe6bac5909edc0
louise-guillon <louise.guillon.fondimare@gmail.com>
2022-03-23 11:44:32 +0100

    bug fix

--- deep_folding/brainvisa/resample_files.py
+++ deep_folding/brainvisa/resample_files.py
@@ -224,7 +224,7 @@ class FileResampler:
         if os.path.exists(src_file):
             resampled = self.resample_one_subject(
                 src_file=src_file,
-                out_voxel_size=self.out_voxel,
+                out_voxel_size=self.out_voxel_size,
                 transform_file=transform_file)
             aims.write(resampled, resampled_file)
         else:
