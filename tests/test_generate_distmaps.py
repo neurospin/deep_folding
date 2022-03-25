c47d622f2539a35ca62db14583f329adf3ce128f
Joel Chavas <joel.chavas@gmail.com>
2022-03-25 17:02:22 +0100

    Renames distmap -> distmaps for consistency

--- tests/test_generate_distmaps.py
+++ tests/test_generate_distmaps.py
@@ -1,16 +1,16 @@
-from deep_folding.brainvisa import generate_distmap
+from deep_folding.brainvisa import generate_distmaps
 from deep_folding.brainvisa.utils.constants import _ALL_SUBJECTS
 
 distmaps_dir = 'data/test'
 
-def test_generate_distmap_help():
+def test_generate_distmaps_help():
     args = "--help"
     argv = args.split(' ')
-    generate_distmap.main(argv)
+    generate_distmaps.main(argv)
 
 
-def test_generate_distmap_n_0():
+def test_generate_distmaps_n_0():
     """Tests the function when number of subjects is 0"""
-    generate_distmap.generate_distmaps(
+    generate_distmaps.generate_distmaps(
         distmaps_dir=distmaps_dir,
         number_subjects=0)
