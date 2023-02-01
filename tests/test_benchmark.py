6a0a4191c5b2ed96c8262805a6a37e58e6d26b5f
Joel Chavas <joel.chavas@gmail.com>
2023-02-01 21:53:57 +0100

    Improves tests

--- tests/test_benchmark.py
+++ tests/test_benchmark.py
@@ -1,7 +1,5 @@
 from soma import aims
 import numpy as np
-from glob import glob
-import random
 import pandas as pd
 import os
 from deep_folding.brainvisa.benchmark_generation_distmap import Benchmark
@@ -27,6 +25,26 @@ def equal_csv_files(csv1, csv2):
     equal_csv = csv1.equals(csv2)
     return equal_csv
 
+def are_arrays_almost_equal(arr1, arr2, epsilon, max_number_different_pixels):
+	"""Returns True if arrays arr1 and arr2 are almost equal
+
+	arr1 and arr2 are almost equal if at most max_number_different_pixels pixels
+	differ by more than epsilon
+
+	Args:
+		arr1: first numpy array
+		arr2: second numpy array
+		epsilon: max allowed difference between pixels values
+		max_number_different_pixels: max allowed different number of pixels
+
+	Returns:
+		equal_arrays: True if arrays are almost equal
+		number_different_pixels: number of pixels differing by more thanepsilon
+    """
+	difference = (abs(arr1 - arr2) >= epsilon)
+	number_different_pixels = np.count_nonzero(difference)
+	equal_arrays = number_different_pixels <= max_number_different_pixels
+	return equal_arrays, number_different_pixels
 
 def test_suppr_benchmark():
     """Tests suppr benchmark generation
@@ -69,8 +87,11 @@ def test_suppr_benchmark():
     skel_ref = aims.read(os.path.join(
         ref_dir, 'skeleton_100206_suppr.nii.gz')).arraydata()
 
-    equal_skel = equal_skeletons(skel_ref, skel_target)
-    # equal_skel,_ = are_arrays_almost_equal(skel_ref, skel_target, 2, 1000)
+    # equal_skel = equal_skeletons(skel_ref, skel_target)
+    equal_skel, nb_different_pixels = are_arrays_almost_equal(skel_ref, skel_target, 1, 0)
+    print(f"skel_target: {skel_target}")
+    print(f"skel_ref: {skel_ref}")
+    print(f"nb of different pixels: {nb_different_pixels}")
     assert equal_skel
 
     tgt_csv = os.path.join(
